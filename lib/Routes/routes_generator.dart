6197d87561c2deede50865ac4e9cc17e2cb26512
Vijay <vijaythevirtue@gmail.com>
2022-02-14 15:59:31 +0530

    enhancement

---
+++ lib/Routes/routes_generator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../Screens/home_page.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => const MyHomePage(title: 'Flutter Animations'),
+        );
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/Screens/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:animated_background/animated_background.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: AnimatedBackground(
+        behaviour: RandomParticleBehaviour(
+          options: const ParticleOptions(
+            spawnMaxRadius: 50,
+            spawnMinSpeed: 10.00,
+            particleCount: 68,
+            spawnMaxSpeed: 50,
+            minOpacity: 0.3,
+            spawnOpacity: 0.4,
+            baseColor: Colors.blue,
+            image: Image(image: AssetImage('assets/Images/Flutter.png')),
+          ),
+        ),
+        vsync: this,
+        child: const SizedBox(),
+      ),
+    );
+  }
+}
